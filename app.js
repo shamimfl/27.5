634822188aac63d31b84dfb25905e3904c06317c
shamimfl <fl.shamim69@gmail.com>
2022-08-19 16:06:17 +0600

    frist commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// task 1
+
+// document.getElementById('penguin').addEventListener('click', function(){
+//     console.log('connented')
+// })
+// task 2
+
+document.getElementById('penguin').addEventListener('click', function(){
+    const text = document.getElementById('text');
+    text.innerText = 'i Am penguin'
+})
+
+
+
+// task 3 
+
